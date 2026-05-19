00daeb1983feb8b4634a07643b890d898d617325
Marvin Juárez <25001064@falileo.edu>
2026-05-18 18:20:59 -0600

    semana5

--- config/db.js
+++ config/db.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const mysql = require("mysql2/promise");
 
 
 
@@ -8,9 +9,26 @@ const connectDB = async () => {
         console.log('MongoDB connected successfully');
     } catch (err) {
         console.error('MongoDB connection error:', err);
+        process.exit(1); // Exit process with failure
+    }
+};
+
+
+const connectMySQL = async () => {
+    try {
+        const pool = mysql.createPool({
+            host: "localhost",
+            port: 3306,
+            user: "app_user",
+            password: "app_password",
+            database: "app_db",
+        });
+        console.log('MySQL connected successfully');
+        return pool;
+    } catch (err) {
+        console.error('MySQL connection error:', err);
         process.exit(1);
     }
 };
 
-module.exports = { connectDB };
-
+module.exports = { connectDB, connectMySQL };
